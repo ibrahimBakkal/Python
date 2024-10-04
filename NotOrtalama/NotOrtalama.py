ce3443f6c92e447fa82160c52de4b611a0e6ff77
ibrahimBakkal <ibakkal25@gmail.com>
2024-10-04 21:16:46 +0300

    Update NotOrtalama.py

--- NotOrtalama/NotOrtalama.py
+++ NotOrtalama/NotOrtalama.py
@@ -3,8 +3,8 @@ nmr=input("Öğrenci numaranızı giriniz: ")
 vize=input("Vİze notunuzu giriniz:")
 final=input("Final notunuzu giriniz: ")
 ort=int(vize)*0.4+int(final)*0.6
-if int(final)>=55 and ort>=40:
-    print("İnönü üniversitesi öğrenci "+nmr+" numaralı "+ogr+"'ın ortalaması "+str(ort)+"dır\nŞartlı olarak dersten geçmiştir.")
+if int(final)>=55 and ort>=40 and ort<45:
+    print(f"İnönü üniversitesi {nmr} öğrenci numaralı {ogr}'ın ortalaması {ort} dır\nŞartlı olarak dersten geçmiştir.")
 elif int(final)>=55 and ort>=45:
     print("İnönü üniversitesi öğrenci "+nmr+" numaralı "+ogr+"'ın ortalaması "+str(ort)+"dır\nDersten geçmiştir.")
 else:
